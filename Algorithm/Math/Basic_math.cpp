764c15e6c5972739bb405eabb0d2aa7ec34cd9e2
HyoKyuKang <154680160+HyoKyuKang@users.noreply.github.com>
2026-07-23 22:35:48 +0900

    Update Basic_math.cpp

--- Algorithm/Math/Basic_math.cpp
+++ Algorithm/Math/Basic_math.cpp
@@ -1,5 +1,5 @@
 
-ll R[n_], L[n_];
+ll R[nmax], L[nmax];
 ll pw(ll a, ll b) {
 	ll ret = 1;
 	while (b) {
@@ -16,9 +16,9 @@ ll ncr(ll n, ll r) { return R[n] * L[r] % mod * L[n - r] % mod; }
 ll nhr(ll n, ll r) { return ncr(n + r - 1, r); }
 void solve() {
 	R[0] = 1;
-	for (int i = 1; i < n_; i++) R[i] = R[i - 1] * i % mod;
-	L[n_-1] = inv(R[n_-1]);
-	for (int i = n_ - 2; i >= 0; i--) L[i] = L[i + 1] * (i + 1) % mod;
+	for (int i = 1; i < nmax; i++) R[i] = R[i - 1] * i % mod;
+	L[nmax - 1] = inv(R[nmax - 1]);
+	for (int i = nmax - 2; i >= 0; i--) L[i] = L[i + 1] * (i + 1) % mod;
 	cin >> m;
 	while (m--) {
 		cin >> a >> b;
